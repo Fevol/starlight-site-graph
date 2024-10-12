47811cbd49ec6425a37e197d3a19f125aca5133d
Fevol <8179397+Fevol@users.noreply.github.com>
2024-10-13 00:22:56 +0200

    refactor: create Backlinks and PageBacklinks components for symmetry with Graph components refactor: removed starlight type imports in graph/backlinks components
    BREAKING CHANGE: PageSidebar now needs to use PageBacklinks

--- packages/starlight-site-graph/components/index.ts
+++ packages/starlight-site-graph/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Graph } from './graph/Graph.astro';
 export { default as PageGraph } from './graph/PageGraph.astro';
-export { default as Backlinks } from './Backlinks.astro';
+export { default as Backlinks } from './backlinks/Backlinks.astro';
+export { default as PageBacklinks } from './backlinks/PageBacklinks.astro';
