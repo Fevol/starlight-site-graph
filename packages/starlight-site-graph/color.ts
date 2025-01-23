18b738e8e4ae1a28e9c9d9dcb30d90bff2c72a75
Fevol <8179397+fevol@users.noreply.github.com>
2025-01-23 16:12:11 +0100

    fix: convert provided css variables to rgb color space for pixi compatibility (fixes #7)

--- packages/starlight-site-graph/color.ts
+++ packages/starlight-site-graph/color.ts
@@ -1,3 +1,5 @@
+import chroma from 'chroma-js';
+
 export type GraphColorConfig = {
 	backgroundColor: string;
 
@@ -31,37 +33,41 @@ export type GraphColorConfig = {
 	labelColorMuted: string;
 };
 
+function getStyleColorProperty(style: CSSStyleDeclaration, property: string): string {
+	return chroma(style.getPropertyValue(property)).hex();
+}
+
 export function getGraphColors(node: HTMLElement): GraphColorConfig {
 	const style = getComputedStyle(node);
 	return {
-		backgroundColor: style.getPropertyValue('--slsg-graph-bg-color'),
+		backgroundColor: getStyleColorProperty(style, '--slsg-graph-bg-color'),
 
-		nodeColor: style.getPropertyValue('--slsg-node-color'),
-		nodeColorHover: style.getPropertyValue('--slsg-node-color-hover'),
-		nodeColorAdjacent: style.getPropertyValue('--slsg-node-color-adjacent'),
-		nodeColorMuted: style.getPropertyValue('--slsg-node-color-muted'),
+		nodeColor: getStyleColorProperty(style, '--slsg-node-color'),
+		nodeColorHover: getStyleColorProperty(style, '--slsg-node-color-hover'),
+		nodeColorAdjacent: getStyleColorProperty(style, '--slsg-node-color-adjacent'),
+		nodeColorMuted: getStyleColorProperty(style, '--slsg-node-color-muted'),
 
-		nodeColorCurrent: style.getPropertyValue('--slsg-node-color-current'),
-		nodeColorVisited: style.getPropertyValue('--slsg-node-color-visited'),
-		nodeColorUnresolved: style.getPropertyValue('--slsg-node-color-unresolved'),
-		nodeColorExternal: style.getPropertyValue('--slsg-node-color-external'),
-		nodeColorTag: style.getPropertyValue('--slsg-node-color-tag'),
-		nodeColor1: style.getPropertyValue('--slsg-node-color-1'),
-		nodeColor2: style.getPropertyValue('--slsg-node-color-2'),
-		nodeColor3: style.getPropertyValue('--slsg-node-color-3'),
-		nodeColor4: style.getPropertyValue('--slsg-node-color-4'),
-		nodeColor5: style.getPropertyValue('--slsg-node-color-5'),
-		nodeColor6: style.getPropertyValue('--slsg-node-color-6'),
-		nodeColor7: style.getPropertyValue('--slsg-node-color-7'),
-		nodeColor8: style.getPropertyValue('--slsg-node-color-8'),
-		nodeColor9: style.getPropertyValue('--slsg-node-color-9'),
+		nodeColorCurrent: getStyleColorProperty(style, '--slsg-node-color-current'),
+		nodeColorVisited: getStyleColorProperty(style, '--slsg-node-color-visited'),
+		nodeColorUnresolved: getStyleColorProperty(style, '--slsg-node-color-unresolved'),
+		nodeColorExternal: getStyleColorProperty(style, '--slsg-node-color-external'),
+		nodeColorTag: getStyleColorProperty(style, '--slsg-node-color-tag'),
+		nodeColor1: getStyleColorProperty(style, '--slsg-node-color-1'),
+		nodeColor2: getStyleColorProperty(style, '--slsg-node-color-2'),
+		nodeColor3: getStyleColorProperty(style, '--slsg-node-color-3'),
+		nodeColor4: getStyleColorProperty(style, '--slsg-node-color-4'),
+		nodeColor5: getStyleColorProperty(style, '--slsg-node-color-5'),
+		nodeColor6: getStyleColorProperty(style, '--slsg-node-color-6'),
+		nodeColor7: getStyleColorProperty(style, '--slsg-node-color-7'),
+		nodeColor8: getStyleColorProperty(style, '--slsg-node-color-8'),
+		nodeColor9: getStyleColorProperty(style, '--slsg-node-color-9'),
 
-		linkColor: style.getPropertyValue('--slsg-link-color'),
-		linkColorHover: style.getPropertyValue('--slsg-link-color-hover'),
-		linkColorMuted: style.getPropertyValue('--slsg-link-color-muted'),
+		linkColor: getStyleColorProperty(style, '--slsg-link-color'),
+		linkColorHover: getStyleColorProperty(style, '--slsg-link-color-hover'),
+		linkColorMuted: getStyleColorProperty(style, '--slsg-link-color-muted'),
 
-		labelColor: style.getPropertyValue('--slsg-label-color'),
-		labelColorHover: style.getPropertyValue('--slsg-label-color-hover'),
-		labelColorMuted: style.getPropertyValue('--slsg-label-color-muted'),
+		labelColor: getStyleColorProperty(style, '--slsg-label-color'),
+		labelColorHover: getStyleColorProperty(style, '--slsg-label-color-hover'),
+		labelColorMuted: getStyleColorProperty(style, '--slsg-label-color-muted'),
 	};
 }
