bbba033ac8f4955e50f8af105969afb07d90c492
Fevol <8179397+fevol@users.noreply.github.com>
2025-10-12 23:23:31 +0200

    refactor: inline icons into code instead of raw imports fix: prevent mime error from icon imports when dev-ing

--- packages/starlight-site-graph/components/elements/icons.ts
+++ packages/starlight-site-graph/components/elements/icons.ts
@@ -1,41 +1,110 @@
-import minimize from '../../assets/svgs/minimize.svg?raw';
-import maximize from '../../assets/svgs/maximize.svg?raw';
-import graph0 from '../../assets/svgs/graph-0.svg?raw';
-import graph1 from '../../assets/svgs/graph-1.svg?raw';
-import graph2 from '../../assets/svgs/graph-2.svg?raw';
-import graph3 from '../../assets/svgs/graph-3.svg?raw';
-import graph4 from '../../assets/svgs/graph-4.svg?raw';
-import graph5 from '../../assets/svgs/graph-5.svg?raw';
-import focus from '../../assets/svgs/focus.svg?raw';
-import arrow from '../../assets/svgs/arrow.svg?raw';
-import line from '../../assets/svgs/line.svg?raw';
-import settings from '../../assets/svgs/settings.svg?raw';
-import link from '../../assets/svgs/link.svg?raw';
-import unlink from '../../assets/svgs/unlink.svg?raw';
-import resolved from '../../assets/svgs/resolved.svg?raw';
-import unresolved from '../../assets/svgs/unresolved.svg?raw';
-
 export const icons = {
-	minimize: minimize,
-	maximize: maximize,
+	minimize: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-2">
+    <polyline points="4 14 10 14 10 20"/>
+    <polyline points="20 10 14 10 14 4"/>
+    <line x1="14" x2="21" y1="10" y2="3"/>
+    <line x1="3" x2="10" y1="21" y2="14"/>
+</svg>`,
+	maximize: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-2">
+    <polyline points="15 3 21 3 21 9"/>
+    <polyline points="9 21 3 21 3 15"/>
+    <line x1="21" x2="14" y1="3" y2="10"/>
+    <line x1="3" x2="10" y1="21" y2="14"/>
+</svg>`,
+	graph0: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m29 17c-4.87-0.016-7.9415 3.4995-8 8 0 1.846 0.634 3.542 1.688 4.897 1.2592 1.6856 3.4469 2.7789 5.313 3.033 0.82721 0.14691 1.1875 0.10527 2 0 1.458-0.183 2.792-0.759 3.898-1.619 1.8455-1.3273 3.0715-4.0458 3.101-6.311-0.016478-4.4809-3.959-7.9475-8-8zm0 14c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/>
+ </g>
+</svg>`,
+	graph1: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m29 17c-1.846 0-3.542 0.634-4.898 1.688l-7.669-7.669c0.352-0.595 0.567-1.279 0.567-2.019 0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4c0.74 0 1.424-0.215 2.019-0.567l7.669 7.669c-1.054 1.356-1.688 3.052-1.688 4.898s0.634 3.542 1.688 4.897c1.2592 1.6856 3.4469 2.7789 5.313 3.033 0.82721 0.14691 1.1875 0.10527 2 0 1.458-0.183 2.792-0.759 3.898-1.619 1.8455-1.3273 3.0715-4.0458 3.101-6.311-0.016478-4.4809-3.959-7.9475-8-8zm-18-8c0-1.103 0.897-2 2-2s2 0.897 2 2-0.897 2-2 2-2-0.897-2-2zm18 22c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/>
+ </g>
+</svg>`,
+	graph2: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m29 17c-1.846 0-3.542 0.634-4.898 1.688l-7.669-7.669c0.352-0.595 0.567-1.279 0.567-2.019 0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4c0.74 0 1.424-0.215 2.019-0.567l7.669 7.669c-1.054 1.356-1.688 3.052-1.688 4.898s0.634 3.542 1.688 4.897c1.2592 1.6856 3.4469 2.7789 5.313 3.033v10.16c-2.833 0.478-5 2.942-5 5.91 0 3.309 2.691 6 6 6s6-2.691 6-6c0-2.967-2.167-5.431-5-5.91v-10.16c1.458-0.183 2.792-0.759 3.898-1.619 1.8455-1.3273 3.0715-4.0458 3.101-6.311-0.016478-4.4809-3.959-7.9475-8-8zm-18-8c0-1.103 0.897-2 2-2s2 0.897 2 2-0.897 2-2 2-2-0.897-2-2zm22 40c0 2.206-1.794 4-4 4s-4-1.794-4-4 1.794-4 4-4 4 1.794 4 4zm-4-18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z"/>
+ </g>
+</svg>`,
+	graph3: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m29 17c-1.846 0-3.542 0.634-4.898 1.688l-7.669-7.669c0.352-0.595 0.567-1.279 0.567-2.019 0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4c0.74 0 1.424-0.215 2.019-0.567l7.669 7.669c-1.054 1.356-1.688 3.052-1.688 4.898s0.634 3.542 1.688 4.897c1.2592 1.6856 3.4469 2.7789 5.313 3.033v10.16c-2.833 0.478-5 2.942-5 5.91 0 3.309 2.691 6 6 6s6-2.691 6-6c0-2.967-2.167-5.431-5-5.91v-10.16c1.458-0.183 2.792-0.759 3.898-1.619l7.669 7.669c-0.353 0.596-0.568 1.28-0.568 2.02 0 2.206 1.794 4 4 4s4-1.794 4-4-1.794-4-4-4c-0.74 0-1.424 0.215-2.019 0.567l-7.669-7.669c1.054-1.356 1.688-3.052 1.688-4.898-0.37585-4.4888-3.959-7.9475-8-8zm-18-8c0-1.103 0.897-2 2-2s2 0.897 2 2-0.897 2-2 2-2-0.897-2-2zm22 40c0 2.206-1.794 4-4 4s-4-1.794-4-4 1.794-4 4-4 4 1.794 4 4zm-4-18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6zm18 10c0 1.103-0.897 2-2 2s-2-0.897-2-2 0.897-2 2-2 2 0.897 2 2z"/>
+ </g>
+</svg>`,
+	graph4: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m49 0c-3.309 0-6 2.691-6 6 0 1.035 0.263 2.009 0.726 2.86l-9.829 9.829c-1.355-1.055-3.051-1.689-4.897-1.689s-3.542 0.634-4.898 1.688l-7.669-7.669c0.352-0.595 0.567-1.279 0.567-2.019 0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4c0.74 0 1.424-0.215 2.019-0.567l7.669 7.669c-1.054 1.356-1.688 3.052-1.688 4.898s0.634 3.542 1.688 4.897c1.2592 1.6856 3.4469 2.7789 5.313 3.033v10.16c-2.833 0.478-5 2.942-5 5.91 0 3.309 2.691 6 6 6s6-2.691 6-6c0-2.967-2.167-5.431-5-5.91v-10.16c1.458-0.183 2.792-0.759 3.898-1.619l7.669 7.669c-0.353 0.596-0.568 1.28-0.568 2.02 0 2.206 1.794 4 4 4s4-1.794 4-4-1.794-4-4-4c-0.74 0-1.424 0.215-2.019 0.567l-7.669-7.669c1.054-1.356 1.688-3.052 1.688-4.898s-0.634-3.542-1.688-4.897l9.665-9.665c1.065 0.967 2.474 1.562 4.023 1.562 3.309 0 6-2.691 6-6s-2.691-6-6-6zm-38 9c0-1.103 0.897-2 2-2s2 0.897 2 2-0.897 2-2 2-2-0.897-2-2zm22 40c0 2.206-1.794 4-4 4s-4-1.794-4-4 1.794-4 4-4 4 1.794 4 4zm-4-18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6zm18 10c0 1.103-0.897 2-2 2s-2-0.897-2-2 0.897-2 2-2 2 0.897 2 2zm2-31c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z"/>
+ </g>
+</svg>`,
+	graph5: `<svg width="55" height="55" version="1.1" viewBox="0 0 14.552 14.552" xmlns="http://www.w3.org/2000/svg">
+ <g transform="scale(.26458)" fill="currentColor">
+  <path d="m49 0c-3.309 0-6 2.691-6 6 0 1.035 0.263 2.009 0.726 2.86l-9.829 9.829c-1.355-1.055-3.051-1.689-4.897-1.689s-3.542 0.634-4.898 1.688l-7.669-7.669c0.352-0.595 0.567-1.279 0.567-2.019 0-2.206-1.794-4-4-4s-4 1.794-4 4 1.794 4 4 4c0.74 0 1.424-0.215 2.019-0.567l7.669 7.669c-1.054 1.356-1.688 3.052-1.688 4.898s0.634 3.542 1.688 4.897l-12.664 12.665c-1.066-0.967-2.475-1.562-4.024-1.562-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6c0-1.035-0.263-2.009-0.726-2.86l12.829-12.829c1.106 0.86 2.44 1.436 3.898 1.619v10.16c-2.833 0.478-5 2.942-5 5.91 0 3.309 2.691 6 6 6s6-2.691 6-6c0-2.967-2.167-5.431-5-5.91v-10.16c1.458-0.183 2.792-0.759 3.898-1.619l7.669 7.669c-0.353 0.596-0.568 1.28-0.568 2.02 0 2.206 1.794 4 4 4s4-1.794 4-4-1.794-4-4-4c-0.74 0-1.424 0.215-2.019 0.567l-7.669-7.669c1.054-1.356 1.688-3.052 1.688-4.898s-0.634-3.542-1.688-4.897l9.665-9.665c1.065 0.967 2.474 1.562 4.023 1.562 3.309 0 6-2.691 6-6s-2.691-6-6-6zm-38 9c0-1.103 0.897-2 2-2s2 0.897 2 2-0.897 2-2 2-2-0.897-2-2zm-5 42c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm27-2c0 2.206-1.794 4-4 4s-4-1.794-4-4 1.794-4 4-4 4 1.794 4 4zm-4-18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6zm18 10c0 1.103-0.897 2-2 2s-2-0.897-2-2 0.897-2 2-2 2 0.897 2 2zm2-31c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z"/>
+ </g>
+</svg>`,
 
-	graph0: graph0,
-	graph1: graph1,
-	graph2: graph2,
-	graph3: graph3,
-	graph4: graph4,
-	graph5: graph5,
+	focus: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-focus">
+    <circle cx="12" cy="12" r="3"/>
+    <path d="M3 7V5a2 2 0 0 1 2-2h2"/>
+    <path d="M17 3h2a2 2 0 0 1 2 2v2"/>
+    <path d="M21 17v2a2 2 0 0 1-2 2h-2"/>
+    <path d="M7 21H5a2 2 0 0 1-2-2v-2"/>
+</svg>`,
 
-	focus: focus,
+	arrow: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="5" cy="5" r="3"/>
+    <path d="M8 5h9a2 2 0 0 1 2 2v8"/>
+    <path d="m12 8-3-3 3-3"/>
+    <circle cx="19" cy="19" r="3"/>
+</svg>`,
+	line: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="5" cy="5" r="3"/>
+    <path d="M8 5h9a2 2 0 0 1 2 2v8"/>
+    <circle cx="19" cy="19" r="3"/>
+</svg>`,
 
-	arrow: arrow,
-	line: line,
+	link: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
+    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
+</svg>`,
+	unlink: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="m18.84 12.25 1.72-1.71h-.02a5.004 5.004 0 0 0-.12-7.07 5.006 5.006 0 0 0-6.95 0l-1.72 1.71"/>
+    <path d="m5.17 11.75-1.71 1.71a5.004 5.004 0 0 0 .12 7.07 5.006 5.006 0 0 0 6.95 0l1.71-1.71"/>
+    <line x1="8" x2="8" y1="2" y2="5"/>
+    <line x1="2" x2="5" y1="8" y2="8"/>
+    <line x1="16" x2="16" y1="19" y2="22"/>
+    <line x1="19" x2="22" y1="16" y2="16"/>
+</svg>`,
 
-	link: link,
-	unlink: unlink,
+	resolved: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="11" cy="11" r="8"/>
+    <path d="m21 21-4.3-4.3"/>
+</svg>`,
+	unresolved: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="m13.5 8.5-5 5"/>
+    <path d="m8.5 8.5 5 5"/>
+    <circle cx="11" cy="11" r="8"/>
+    <path d="m21 21-4.3-4.3"/>
+</svg>`,
 
-	resolved: resolved,
-	unresolved: unresolved,
-
-	settings: settings,
+	settings: `<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+  <circle cx="12" cy="12" r="3" />
+</svg>`,
 } as const satisfies Record<string, string>;
